9b0e18aff021a7c74f7a7df5b11aa0b607db69ab
Chris Wright <cawright.99@gmail.com>
2018-10-24 00:49:49 +1100

    First commit

---
+++ user-data.rkt
@@ -0,0 +1,30 @@
+(module user-data racket
+  (provide h1 h2 h3)
+
+  (define h1 #<<;
+You've just arrived in Ward 3W, respoinding to a call
+from the nurse looking after Ms Jones:
+
+"Thank heavens you're here - she's really confused and I
+think she's had a stroke."
+
+;
+    )
+
+  (define h2 #<<;
+One of the patient's relatives rushes out of the room and
+says "Doctor - please help my mother -- she's dying!"
+
+;
+    )
+
+  (define h3 #<<;
+The nurse tells you that Ms Jones is a 61 year old woman admitted
+4 days ago for an umbilical hernia repair. She's got a past history of
+hypertension, and is taking Enalapril 10mg daily, and is allergic to
+penicillin
+
+;
+    )
+
+)
